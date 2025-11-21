b75281a73cb1714cd6f6e9767844e250603fd2f0
aminmatlo <devkarim60@gmail.com>
2025-11-21 22:10:58 +0100

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,11 +1,27 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+import requests
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return 'Hello, World!'
+@app.route("/live", methods=["GET"])
+def live_api():
+    params = request.args.to_dict()
+    url = "https://version.ffmax.purplevioleto.com/live/ver.php"
 
-@app.route('/about')
-def about():
-    return 'About'
+    headers = {
+        'User-Agent': "Dalvik/2.1.0 (Linux; U; Android 15; SM-A165F Build/AP3A.240905.015.A2)",
+        'Connection': "Keep-Alive",
+        'Accept-Encoding': "gzip",
+        'X-Unity-Version': "2018.4.11f1"
+    }
+
+    try:
+        r = requests.get(url, headers=headers, params=params)
+        data = r.json()
+        data["server_url"] = "https://loginbp.karim.com/"
+        return jsonify(data)
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
