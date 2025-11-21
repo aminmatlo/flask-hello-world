a8547c510d4a741c3b1fa4dafd2c455f5ce25b34
aminmatlo <devkarim60@gmail.com>
2025-11-21 23:36:57 +0100

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,26 +1,11 @@
 from flask import Flask, request, jsonify
-import requests
 
 app = Flask(__name__)
 
-@app.route("/live", methods=["GET"])
-def live_api():
+@app.route('/live/', methods=['GET'])
+def live():
+    # أخذ جميع الـ query parameters وتحويلها إلى dict
     params = request.args.to_dict()
-    url = "https://version.ffmax.purplevioleto.com/live/ver.php"
-
-    headers = {
-        'User-Agent': "Dalvik/2.1.0 (Linux; U; Android 15; SM-A165F Build/AP3A.240905.015.A2)",
-        'Connection': "Keep-Alive",
-        'Accept-Encoding': "gzip",
-        'X-Unity-Version': "2018.4.11f1"
-    }
-
-    try:
-        r = requests.get(url, headers=headers, params=params, timeout=10)
-        data = r.json()
-        data["server_url"] = "https://loginbp.karim.com/"
-        return jsonify(data)
-    except requests.exceptions.RequestException as e:
-        return jsonify({"error": "Request failed", "details": str(e)}), 500
-    except ValueError:
-        return jsonify({"error": "Response is not valid JSON"}), 500
+    
+    # إرجاعهم على شكل JSON
+    return jsonify(params)
