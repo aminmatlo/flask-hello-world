0b92d72d4c3945264fd81bfec9a2cc4cbbe1c28f
aminmatlo <devkarim60@gmail.com>
2025-11-22 02:25:17 +0100

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,28 +1,20 @@
-from flask import Flask, jsonify
+from flask import Flask, request, jsonify
 import requests
 
 app = Flask(__name__)
 
 @app.route('/live/ver.php')
 def live_ver():
-    url = (
-        "https://version.ffmax.purplevioleto.com/live/ver.php?"
-        "version=2.117.9&lang=ar&device=android&channel=android_max&"
-        "appstore=googleplay_max&region=ME&release_version=OB51&"
-        "whitelist_version=1.4.0&whitelist_sp_version=1.0.0&"
-        "device_name=samsung%20SM-A165F&device_CPU=ARM64%20FP%20ASIMD%20AES&"
-        "device_GPU=Mali-G57%20MC2&device_mem=3621"
-    )
-
+    params = request.args.to_dict()
+    url = "https://version.ffmax.purplevioleto.com/live/ver.php"
     headers = {
         'User-Agent': "Dalvik/2.1.0 (Linux; U; Android 15; SM-A165F Build/AP3A.240905.015.A2)",
         'Connection': "Keep-Alive",
         'Accept-Encoding': "gzip",
         'X-Unity-Version': "2018.4.11f1"
     }
-
     try:
-        response = requests.get(url, headers=headers)
-        return response.text
+        response = requests.get(url, headers=headers, params=params)
+        return jsonify(response.json())
     except Exception as e:
         return jsonify({"error": str(e)}), 500
